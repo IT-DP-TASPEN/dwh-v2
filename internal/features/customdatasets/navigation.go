7507a7067a5ad014030b36349d032b0a8111aff9
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2026-09-06 10:04:55 +0700

    fix: fixed missing icon

--- internal/features/customdatasets/navigation.go
+++ internal/features/customdatasets/navigation.go
@@ -3,5 +3,5 @@ package customdatasets
 import "github.com/ibldzn/go-admin/internal/platform/navigation"
 
 func Navigation() navigation.Item {
-	return navigation.Item{Key: "custom-datasets", Label: "Custom Datasets", Icon: "table", Path: "/custom-datasets", Permission: PermissionView, Match: navigation.MatchPrefix}
+	return navigation.Item{Key: "custom-datasets", Label: "Custom Datasets", Icon: "table2", Path: "/custom-datasets", Permission: PermissionView, Match: navigation.MatchPrefix}
 }
